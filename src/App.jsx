1406db7f8cc7c4e529bb4db71cf4f62a70bd1af2
Hardik Parekh <hardikparekh7426@gmail.com>
2026-01-20 08:41:13 +0530

    hook_example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import FirstClass from "./CEAB2312/firstclass"
-import Cardcomponent from "./CEAB2312/cardcomponent";
-// import StateExample from "./CE301225/class_state";
-import StringState from "./CE301225/string_state";
-import RenderCond from "./CE301225/render_cond";
-import StateExample from "./CE01012026/state_excersice";
-import ContactBook from "./CEAB6126/contactbook";
-import ProductSelector from "./CE812026/quantity";
-import MultiPart from "./CE13012026/multipart";
-import EasyPagination from "./CE15012026/multiobject";
-import Pagination from "./CE15012026/multiobject";
+// import FirstClass from "./CEAB2312/firstclass"
+// import Cardcomponent from "./CEAB2312/cardcomponent";
+// // import StateExample from "./CE301225/class_state";
+// import StringState from "./CE301225/string_state";
+// import RenderCond from "./CE301225/render_cond";
+// import StateExample from "./CE01012026/state_excersice";
+// import ContactBook from "./CEAB6126/contactbook";
+// import ProductSelector from "./CE812026/quantity";
+// import MultiPart from "./CE13012026/multipart";
+// import EasyPagination from "./CE15012026/multiobject";
+// import Pagination from "./CE15012026/multiobject";
+import Hookexample from "./CE2012026/hook";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
   <StateExample /> */}
   {/* <StateExample /> */}
     {/* <ContactBook />  */}
-    <Pagination/>
+    <Hookexample/>
 
   {/* <ProductSelector/> */}
     </>
